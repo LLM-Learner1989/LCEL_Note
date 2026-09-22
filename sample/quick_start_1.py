af0db3a16ff87d8cf406d7e9eb32efb3604b6859
LLM-Learner1989 <zhangbiaohpu@outlook.com>
2026-09-22 22:35:49 +0800

    Update code

--- sample/quick_start_1.py
+++ sample/quick_start_1.py
@@ -1,4 +1,4 @@
-from langchain_core.runnables import RunnableLambda
+from langchain_core.runnables import RunnableLambda, RunnableParallel
 
 
 def test1(x: int):
@@ -20,8 +20,23 @@ def test2(prompt: str):
         yield item
 
 
+# 3. 流式输出
 streamRunnable = RunnableLambda(test2)
 streamRes = streamRunnable.stream('This is a Dog.')
 
 for chunk in streamRes:
     print(chunk)
+
+# 4. 组合链
+r2 = RunnableLambda(lambda x: x * 2)
+combinedChain = r1 | r2  # 串行
+
+combinedRes = combinedChain.invoke(2)
+print(f"combinedRes: {combinedRes}")
+
+# 5.并行运行
+parallelChain = RunnableParallel(r1=r1, r2=r2)
+
+# max_concurrency: 最大并发数
+parallelRes = parallelChain.batch([1, 2, 3, 4], config={'max_concurrency': 1})
+print(f"parallelRes: {parallelRes}")
