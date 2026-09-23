c544e76a87677f149c16907816156505d2c5d9b1
LLM-Learner1989 <zhangbiaohpu@outlook.com>
2026-09-23 21:28:43 +0800

    Update code

---
+++ sample/quick_start_03.py
@@ -0,0 +1,27 @@
+import time
+
+from langchain_core.tracers import Run
+from langchain_core.runnables import RunnableLambda
+
+
+# 生命周期管理，单位是秒
+def test4(n: int):
+    time.sleep(n)
+    return n * 2
+
+
+def on_start(run_obj: Run):
+    """ 当r1节点启动的时候，自动调用 """
+    print('r1启动的时间： ', run_obj.start_time)
+
+
+def on_end(run_obj: Run):
+    """ 当r1节已经运行结束的时候，自动调用 """
+    print('r1结束的时间： ', run_obj.end_time)
+
+
+r1 = RunnableLambda(test4)
+
+chain = r1.with_listeners(on_start=on_start, on_end=on_end)
+resp = chain.invoke(10)
+print(f"resp: {resp}")
