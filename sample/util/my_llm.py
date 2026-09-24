a77376db0bbbd1dc956c6330c4ae7183cc7c70a7
LLM-Learner1989 <zhangbiaohpu@outlook.com>
2026-09-24 19:26:37 +0800

    Update code

--- sample/util/my_llm.py
+++ sample/util/my_llm.py
@@ -3,8 +3,8 @@ from langchain_deepseek import ChatDeepSeek
 from sample.util.env_utils import DEEPSEEK_API_KEY, DEEPSEEK_API_BASE
 
 # llm = ChatOpenAI(
-#     model="deepseek-v4-flash",
-#     temperature=1.1,
+#     model="deepseek-chat",
+#     temperature=0,
 #     openai_api_key=DEEPSEEK_API_KEY,
 #     openai_api_base=DEEPSEEK_API_BASE,
 # )
